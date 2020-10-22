78125fe7419206f0407b4afbdc4ae00843fda62d
Brian Grinstead <briangrinstead@gmail.com>
2020-10-22 11:15:00 -0700

    graph testing policy data

--- static/common.js
+++ static/common.js
@@ -5,6 +5,18 @@ let METABUGS_URL =
 let LANDINGS_URL =
   "https://community-tc.services.mozilla.com/api/index/v1/task/project.bugbug.landings_risk_report.latest/artifacts/public/landings_by_date.json";
 
+function getCSSVariableValue(name) {
+  return getComputedStyle(document.documentElement).getPropertyValue(name).trim()
+}
+
+let TESTING_TAGS = {
+  "testing-approved": getCSSVariableValue("--green-60"),
+  "testing-exception-elsewhere": getCSSVariableValue("--blue-50"),
+  "testing-exception-ui": getCSSVariableValue("--purple-50"),
+  "testing-exception-unchanged": getCSSVariableValue("--yellow-50"),
+  "testing-exception-other": getCSSVariableValue("--red-50"),
+};
+
 let featureMetabugs = (async function () {
   let response = await fetch(METABUGS_URL, { cache: "no-store" });
   let json = await response.json();
@@ -33,16 +45,30 @@ async function getTestingPolicySummaryData() {
 
   let returnedData = {};
   for (let date of orderedDates) {
-    let result = {};
-    let originalData = data[date];
+    // Ignore data before the testing policy took place.
+    if (
+      temporal.Temporal.Date.compare(
+        temporal.Temporal.Date.from(date),
+        // temporal.Temporal.Date.from("2020-09-15") Once we get the historical data
+        temporal.Temporal.Date.from("2020-10-16")
+      ) < 1
+    ) {
+      continue;
+    }
 
+    let returnedDataForDate = {};
+    for (let tag in TESTING_TAGS) {
+      returnedDataForDate[tag] = 0;
+    }
+
+    let originalData = data[date];
     for (let bug of originalData) {
       for (let tag of bug.testing) {
-        result[tag] = result[tag] ? result[tag] + 1 : 1;
+        returnedDataForDate[tag] = returnedDataForDate[tag] + 1;
       }
     }
 
-    returnedData[date] = result;
+    returnedData[date] = returnedDataForDate;
   }
 
   return returnedData;
